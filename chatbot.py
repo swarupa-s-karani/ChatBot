135a54a1e2b838cdf8a97ab0a5a343b467356f2c
swarupa-s-karani <121108269+swarupa-s-karani@users.noreply.github.com>
2024-08-21 03:15:59 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,120 @@
+import nltk
+# Download NLTK data
+nltk.download('punkt')
+nltk.download('wordnet')
+import json
+
+from nltk.stem import WordNetLemmatizer
+import numpy as np
+import random
+from tensorflow.keras.models import Sequential # type: ignore
+from tensorflow.keras.layers import Dense, Dropout # type: ignore
+from tensorflow.keras.optimizers import SGD # type: ignore
+from tensorflow.keras.models import load_model # type: ignore
+import sys
+with open('data.json') as file:
+    data = json.load(file)
+lemmatizer = WordNetLemmatizer()
+words = []
+classes = []
+documents = []
+ignore_words = ['?', '!', '.', ',']
+# Preprocess data
+for intent in data['intents']:
+    for pattern in intent['patterns']:
+        word_list = nltk.word_tokenize(pattern)
+        words.extend(word_list)
+        documents.append((word_list, intent['tag']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
+words = sorted(list(set(words)))
+classes = sorted(list(set(classes)))
+# Prepare training data
+training = []
+output_empty = [0] * len(classes)
+for doc in documents:
+    bag = []
+    word_patterns = doc[0]
+    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+    for word in words:
+        bag.append(1) if word in word_patterns else bag.append(0)
+    
+    output_row = list(output_empty)
+    output_row[classes.index(doc[1])] = 1
+    
+    training.append([bag, output_row])
+
+random.shuffle(training)
+# Separate the features and labels
+train_x = [np.array(tr[0]) for tr in training]
+train_y = [np.array(tr[1]) for tr in training]
+
+train_x = np.array(train_x)
+train_y = np.array(train_y)
+# Build and train model
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(classes), activation='softmax'))
+
+sgd = SGD(learning_rate=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+model.fit(train_x, train_y, epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.h5')
+# Load model
+model = load_model('chatbot_model.h5')
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+
+def bow(sentence, words, show_details=True):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for s in sentence_words:
+        for i, w in enumerate(words):
+            if w == s:
+                bag[i] = 1
+    return np.array(bag)
+def predict_class(sentence, model):
+    p = bow(sentence, words, show_details=False)
+    res = model.predict(np.array([p]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    return return_list
+
+def get_response(ints, intents_json):
+    if not ints:
+        return "Sorry, I didn't understand that."
+    tag = ints[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if i['tag'] == tag:
+            result = random.choice(i['responses'])
+            break
+    return result
+
+def chatbot_response(msg):
+    ints = predict_class(msg, model)
+    res = get_response(ints, data)
+    return res
+def main():
+    while True:
+        message = input("You: ")
+        if message.lower() == "quit":
+            sys.exit("User requested to quit.")
+        response = chatbot_response(message)
+        print(f"Bot: {response}")
+
+if __name__ == "__main__":
+    main()
